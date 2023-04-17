703f9837e5f8783e8404edb4a05aa101a25973da
ghkdtjsdi <88885019+Yanison@users.noreply.github.com>
2023-04-17 19:17:31 +0900

    section 8

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -12,7 +12,7 @@ public class BeanDefinitionTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     /**
-     * BeanDefinition을 직접 생성해서 스프링 컨테이너에 등록할 수 있다. 하지만 실무에서 직접 정의하거나 사용할 일은 거의 없다.
+     * BeanDefinition을 직접 생성해서 스프링 컨테이너에 등록할 수 있다. 하지만 실무에서 직접 정의하거나 사용할 일은 거의 없다.asdasdas
      */
     @Test
     @DisplayName("빈 설정 메타정보 확인")
